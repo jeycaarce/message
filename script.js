9c179c743e015d33676faa4544afdf66a356fafe
shivam <shivamban30@gmail.com>
2023-12-16 09:51:08 +0530

    🎨✨ Enhanced interactive UI elements for RSVP confirmation
    The changes were implemented to improve the user experience for responding to an RSVP. Specifically, the updates focused on preserving the dimensions of a confirmation GIF and curating a more visually appealing fade-out effect for non-selected options. Additionally, redundant or superfluous animations have been removed, streamlining the UI and minimizing distractions. The `no-btn` event handler was refactored for cleanliness, and clicking `no-btn` now triggers the same evasive behavior as hovering, creating a persistent interactive challenge. Overall, these refinements contribute to a more dynamic and engaging user interface.

--- script.js
+++ script.js
@@ -5,26 +5,27 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
+  gif.style.width = gif.getBoundingClientRect().width + "px";
+  gif.style.height = gif.getBoundingClientRect().height + "px";
   question.innerHTML = "Yay, see you on the 18th!";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  gif.src ="https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  noBtn.style.opacity = "0";
+  yesBtn.style.marginLeft = "0px";
 });
 
-noBtn.addEventListener("mouseover", () => {
-    const noBtnRect = noBtn.getBoundingClientRect();
-    const maxX = window.innerWidth - noBtnRect.width;
-    const maxY = window.innerHeight - noBtnRect.height;
+function handleNoBtn () {
+  noBtn.style.left = noBtn.getBoundingClientRect().x + "px";
+  noBtn.style.top = noBtn.getBoundingClientRect().y + "px";
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+}
 
-    const funMessage = document.querySelector(".fun-message");
-    funMessage.innerHTML = "Can't catch me!";
-    funMessage.style.opacity = 1;
-    setTimeout(() => {
-        funMessage.style.opacity = 0;
-    }, 2000);
-});
+noBtn.addEventListener("mouseover", handleNoBtn);
+noBtn.addEventListener("click", handleNoBtn);
